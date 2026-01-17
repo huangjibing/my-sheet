bea385b13bd33b7330cd60273a13a2ecca6796c9
huangjibing <44466578+huangjibing@users.noreply.github.com>
2026-01-17 15:30:38 +0800

    update 首页修改 配置文件修改

--- components/home/HomeMs.vue
+++ components/home/HomeMs.vue
@@ -14,13 +14,13 @@
 
             <div class="homems-btn">
                 <div class="the-admin" @click="jumpPath('/vigorbuybuy-spreadsheet')">
-                    <span>Browse the Oopbuy Spreadsheet</span>
+                    <span>Browse the Vigorbuy Spreadsheet</span>
                     <i class="icon iconfont icon-youjiantou ml-8"></i>
                 </div>
 
                 <div class="the-lw">
                     <i class="icon iconfont icon-liwuhuodong"></i>
-                    <span class="ml-5">Get Oopbuy Coupons</span>
+                    <span class="ml-5">Get Vigorbuy Coupons</span>
                 </div>
             </div>
 
