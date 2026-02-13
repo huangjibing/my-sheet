6109ba822afb4d017cbb0c896452ea91f7037c85
huangjibing <44466578+huangjibing@users.noreply.github.com>
2026-02-13 10:28:49 +0800

    update 弹窗位置修改

--- components/ProductItem.vue
+++ components/ProductItem.vue
@@ -91,7 +91,7 @@ const shwoDiscord = () => {
 
     <div class="buy-btn pt-2 pb-2 inset-0 theme-btn rounded-full" @click.stop="openGoods">
       <span>Buy on Vigorbuy</span>
-      <i class="icon iconfont icon-tiaozhuan font-13 ml-5 mt-1"></i>
+      <!-- <i class="icon iconfont icon-tiaozhuan font-13 ml-5 mt-1"></i> -->
     </div>
 
     <div class="buy-btn pt-2 pb-2 pl-1 pr-1 border border-[#353535]" @click.stop="shwoDiscord">
--- components/dialog/JoinDiscordDialog.vue
+++ components/dialog/JoinDiscordDialog.vue
@@ -10,7 +10,7 @@ const openDiscord = () => {
 }
 </script>
 <template>
-    <div class="absolute border border-[#000000] rounded-[10px] w-11/12 sm:w-[500px] z-10 p-[20px] bg-[#ffffff] top-2/4 left-2/4 translate-x-[-50%] translate-y-[-50%]">
+    <div class="absolute border border-[#000000] rounded-[10px] w-11/12 sm:w-[500px] z-10 p-[20px] bg-[#ffffff] top-2/4 left-2/4 translate-auto">
         <div class="text-2xl text-center">WHY JOIN DISCORD</div>
 
         <i class="text-3xl icon iconfont icon-guanbi absolute pointer top-[10px] right-[20px]" @click="closeDialog()"></i>
@@ -29,5 +29,7 @@ const openDiscord = () => {
     </div>
 </template>
 <style lang="scss" scoped>
-
+.translate-auto{
+    transform: translateX(-50%) translateY(-50%);
+}
 </style>
